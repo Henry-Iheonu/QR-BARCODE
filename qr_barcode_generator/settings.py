a594db17aab897d7de9cc894660d3312fb104a19
Henry-Iheonu <henryiheonu264@gmail.com>
2024-12-16 13:05:33 +0100

    recent updates 1: type: gunicorn install

--- qr_barcode_generator/settings.py
+++ qr_barcode_generator/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = 'django-insecure-*ji942hsb_1qk8@5dv%l=&_g7d!&^8fo4sg5qpwyb50b!v@dgw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://qr-barcode.onrender.com']
 
 # Application definition
 
